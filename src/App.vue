24f8362c7beeea06787072d6642b6bd86eeb818b
zjh42139 <1095699075@qq.com>
2022-01-13 20:32:08 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="font-bold">
+    用户名:{{ mainStore.name }}
+    <br />
+    长度:{{ mainStore.nameLength }}
+  </div>
+  <hr />
+  <button @click="updateName">修改store中的name</button>
+  <ol>
+    <li>1</li>
+    <li>1</li>
+    <li>1</li>
+  </ol>
+</template>
+
+<script setup lang="ts">
+import { useMainStore } from '@/store'
+
+const mainStore = useMainStore()
+
+const updateName = () => {
+  // $patch 修改 store 中的数据
+  mainStore.$patch({
+    name: '名称被修改了,nameLength也随之改变了'
+  })
+}
+</script>
+
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $test-color;
+  margin-top: 60px;
+}
+</style>
+
