a03896e99bde4834f3c4d46008f4c9ef28e2632b
zjh42139 <1095699075@qq.com>
2022-01-14 10:37:42 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -9,28 +9,28 @@
   <ol>
     <li>1</li>
     <li>1</li>
-    <li>1</li>
+    <li>12</li>
   </ol>
 </template>
 
 <script setup lang="ts">
-  import { useMainStore } from '@/store';
+import { useMainStore } from '@/store';
 
-  const mainStore = useMainStore();
+const mainStore = useMainStore();
 
-  const updateName = () => {
-    // $patch 修改 store 中的数据
-    mainStore.$patch({
-      name: '名称被修改了,nameLength也随之改变了',
-    });
-  };
+const updateName = () => {
+  // $patch 修改 store 中的数据
+  mainStore.$patch({
+    name: '名称被修改了,nameLength也随之改变了',
+  });
+};
 </script>
 
 <style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
 </style>
